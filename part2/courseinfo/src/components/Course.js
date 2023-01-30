71c875548cd660886fa71fd9f3cabc16bc1970c1
ouliana <ouliana.kotik@gmail.com>
2023-01-30 22:41:09 +0300

    exercise 2.3-2.5 complete

--- part2/courseinfo/src/components/Course.js
+++ part2/courseinfo/src/components/Course.js
@@ -1,6 +1,30 @@
-import Header from './Header';
-import Content from './Content';
-import Total from './Total';
+const Header = ({ name }) => <h1>{name}</h1>;
+
+const Part = ({ part }) => (
+  <p>
+    {part.name} {part.exercises}
+  </p>
+);
+
+const Content = ({ parts }) => (
+  <>
+    {parts.map(({ id, ...details }) => (
+      <Part
+        key={id}
+        part={details}
+      />
+    ))}
+  </>
+);
+
+const Total = ({ parts }) => {
+  const total = parts.reduce((sum, current) => sum + current.exercises, 0);
+  return (
+    <p>
+      <strong>total of {total} exercises</strong>
+    </p>
+  );
+};
 
 const Course = ({ course }) => {
   const { name, parts } = course;
