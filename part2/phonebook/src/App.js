d139079d62f727805d405b9222a0f4d2dd746ffa
ouliana <ouliana.kotik@gmail.com>
2023-01-31 18:13:52 +0300

    exercise 2.6 complete

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,59 @@
+import { useState } from 'react';
+
+const Person = ({ name }) => {
+  return <div>{name}</div>;
+};
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]);
+  const [newName, setNewName] = useState('');
+
+  const handleNameChange = event => {
+    setNewName(event.target.value);
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+
+    const isUnique = persons.reduce(
+      (res, curr) => res && curr.name !== newName,
+      true
+    );
+
+    console.log(isUnique);
+    if (isUnique) {
+      const newPerson = {
+        name: newName,
+      };
+      setPersons([...persons, newPerson]);
+    }
+    setNewName('');
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          name:{' '}
+          <input
+            value={newName}
+            onChange={handleNameChange}
+          />
+        </div>
+        <div>
+          <button type='submit'>add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map(person => (
+        <Person
+          key={person.name}
+          name={person.name}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default App;
