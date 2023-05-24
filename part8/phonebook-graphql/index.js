7b70786d79bd8623775c527043b0f1d12e5b4e97
ouliana <ouliana.kotik@gmail.com>
2023-05-24 13:52:07 +0300

    enum, mutations continue

--- part8/phonebook-graphql/index.js
+++ part8/phonebook-graphql/index.js
@@ -38,10 +38,14 @@ type Person {
   address: Address!
   id: ID!
 }
+enum YesNo {
+  YES
+  NO
+}
 
 type Query {
   personCount: Int!
-  allPersons: [Person!]!
+  allPersons(phone:YesNo): [Person!]!
   findPerson(name: String!): Person
 }
 
@@ -52,13 +56,24 @@ type Mutation {
     street: String!
     city: String!
   ): Person
+  editNumber(
+    name: String!
+    phone: String!
+  ): Person
 }
 `;
 
 const resolvers = {
   Query: {
     personCount: () => persons.length,
-    allPersons: () => persons,
+    allPersons: (root, args) => {
+      if (!args.phone) {
+        return persons;
+      }
+      const byPhone = person =>
+        args.phone === 'YES' ? person.phone : !person.phone;
+      return persons.filter(byPhone);
+    },
     findPerson: (root, args) => persons.find(p => p.name === args.name),
   },
   Person: {
@@ -82,6 +97,16 @@ const resolvers = {
       const person = { ...args, id: uuid() };
       persons = persons.concat(person);
       return person;
+    },
+    editNumber: (root, args) => {
+      const person = persons.find(p => p.name === args.name);
+      if (!person) {
+        return null;
+      }
+
+      const updatedPerson = { ...person, phone: args.phone };
+      persons = persons.map(p => (p.name === args.name ? updatedPerson : p));
+      return updatedPerson;
     },
   },
 };
